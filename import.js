58d849e6ba45ae49e7983f2dd5ce17112de08fbe
luke.gumbley <luke.gumbley@gmail.com>
2015-12-06 13:04:08 +1300

    Add rough context and type extraction from filenames.

--- import.js
+++ import.js
@@ -1,8 +1,37 @@
 var fs = require('fs');
 var csv = require('csv');
+var moment = require('moment');
 
-var isCSV = new RegExp('\.[Cc][Ss][Vv]$');
-var files = process.argv.filter(function(p) { return isCSV.test(p); });
+var formats = [{
+	name: 'kb_credit',
+	expression: new RegExp(/(\d{4}-\d{2}-{2}--{4}-\d{4})_(\d{2}[A-Z][a-z]{2}).CSV/),
+	data: function(filename) {
+		if(!this.expression.test(filename)) return null;
+		var match = this.expression.exec(filename);
+		return {
+			filename: filename,
+			type: this.name,
+			account: match[1],
+			date: moment(match[2],'DDMMM').format()
+		};
+	}
+},{
+	name: 'anz_acct',
+	expression: new RegExp(/(\d{2}-\d{4}-\d{7}-\d{2})_Transactions_(\d{4}-\d{2}-\d{2})_(\d{4}-\d{2}-\d{2}).csv/),
+	data: function(filename) {
+		if(!this.expression.test(filename)) return null;
+		var match = this.expression.exec(filename);
+		return {
+			filename: filename,
+			type: this.name,
+			account: match[1],
+			start: moment(match[2]).format(),
+			end: moment(match[3]).format(),
+		};
+	}
+}]
+
+var files = process.argv.filter(function(p) { return !formats.every(function(format) { return !format.expression.test(p); }); });
 
 function readFile(filename, callback) {
 	var parser = csv.parse({columns: true}, function(err, data){
@@ -12,5 +41,8 @@ function readFile(filename, callback) {
 }
 
 files.forEach(function(name) { readFile(name, function(filename, data) {
-	console.log(filename, data.length);
+	var format = formats.find(function(format) { return format.expression.test(filename); });
+	var summary = format.data(filename);
+	summary.lines = data.length;
+	console.log(summary);
 })});
