7aeed17bea217e74d01b8bac7ce538c0310b9a02
Luke Gumbley <luke.gumbley@gmail.com>
2018-01-24 11:38:10 +1300

    Tidy up server startup parameters

--- server.js
+++ server.js
@@ -10,17 +10,32 @@ process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at:', p, 'reason:', reason);
 });
 
-database.connect();
-/*
-database.connect().then(() => {
-	// read all supplied statements
-	process.argv.slice(2).map(filename => parser.parse(filename))
+const options = {
+	sync: false
+};
+
+options.files = process.argv.slice(2).filter(arg => {
+	if(arg === 'sync') {
+		options.sync = true;
+		return false;
+	}
+
+	if(arg.includes('bucket')) {
+		options.bucketFile = arg;
+		return false;
+	}
+
+	return true;
+});
+
+database.connect({ sync: options.sync }).then(() => {
+	options.files.map(filename => parser.parse(filename))
 		.reduce((acc, val) => acc.concat(val), [])
 		.filter(format => format.parsable() !== false)
 		.forEach(format => format.pipe(database.transactions()));
 
-	// read all buckets
-	let csvParser = csv.parse({ columns: true }, (err, buckets) => { database.bucket.bulkCreate(buckets); } );
-	fs.createReadStream('data/buckets.csv').pipe(csvParser);
+	if(options.bucketFile) {
+		let csvParser = csv.parse({ columns: true }, (err, buckets) => { database.bucket.bulkCreate(buckets); } );
+		fs.createReadStream(options.bucketFile).pipe(csvParser);
+	}
 }).catch(function(err) { console.log(err); });
-*/
