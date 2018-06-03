3d73a182e558cb614dd17871f5e283b0d90071f9
Luke Gumbley <luke.gumbley@gmail.com>
2018-06-03 15:05:02 +1200

    Fix issues with unmanaged input in BucketPicker

--- app/components/BucketPicker.js
+++ app/components/BucketPicker.js
@@ -2,24 +2,46 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class BucketPicker extends Component {
-	constructor() {
-		super();
-		this.handleBlur = this.handleBlur.bind(this);
+	constructor(props) {
+		super(props);
+		this.state = { value: props.bucket.name, isDirty: false };
 	}
 
-	handleBlur(e) {
-		if(e.target.value !== (this.props.bucket.name || '') && this.props.onChange)
-			this.props.onChange(e.target.value);
+	handleChange = (event) => {
+		const value = event.target.value;
+		this.setState({ value, isDirty: value !== this.props.bucket.name });
+	};
+
+	handleKeyPress = (event) => {
+		if(event.key=='Enter') event.currentTarget.blur();
+	};
+
+	handleBlur = (event) => {
+		if(this.props.onChange && this.state.value !== this.props.bucket.name)
+			this.props.onChange(this.state.value);
+	};
+
+	static getDerivedStateFromProps(props, state) {
+		return state.isDirty
+			? null
+			: { value: props.bucket.name, isDirty: false };
 	}
 
 	render() {
-		return (<input type="text" list="buckets" name="bucket" onBlur={this.handleBlur} defaultValue={this.props.bucket.name} />);
+		return (<input
+			type="text"
+			list="buckets"
+			name="bucket"
+			onBlur={this.handleBlur}
+			onChange={this.handleChange}
+			onKeyPress={this.handleKeyPress}
+			value={this.state.value} />);
 	}
 }
 
 function mapStateToProps(state, props) {
 	return {
-		bucket: state.buckets.items[props.bucketId] || {}
+		bucket: state.buckets.items[props.bucketId] || { name: '' }
 	};
 }
 
