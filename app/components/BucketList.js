d173cd91236b864cc44f21102ecc82b2c4975e52
Luke Gumbley <luke.gumbley@gmail.com>
2018-06-20 19:13:17 +1200

    Convert bucket list to react-virtualized Table
    - Drop Bucket component
    - Protect rendering against non-periodic buckets
    - Remove div outline on focus
    - Colour GlyphButtons green
    - Update and correctly indent TODOs

--- app/components/BucketList.js
+++ app/components/BucketList.js
@@ -1,22 +1,82 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Bucket from './Bucket';
+import { Column, Table, AutoSizer } from 'react-virtualized';
+import 'react-virtualized/styles.css';
+import moment from 'moment';
+
 import GlyphButton from './GlyphButton';
-import { editBucket } from '../actions/buckets.js';
+import { editBucket, deleteBucket } from '../actions/buckets.js';
 import { getBuckets } from '../selectors/buckets.js';
 
 class BucketList extends Component {
-	render() {
-		var buckets = this.props.buckets.map(bucket => {
-			return (<Bucket key={bucket.id} bucket={bucket} />);
-		});
+
+	rowGetter = ({ index }) => {
+		const bucket = this.props.buckets[index];
+
+		const isPeriodic = bucket.nextDate.isValid() && bucket.period > 0;
+
+		const prev = moment(bucket.nextDate).subtract(bucket.period,bucket.periodUnit);
+		const days = bucket.nextDate.diff(prev,'days', true);
+		const age = moment().diff(bucket.zeroDate, 'days', true);
+
+		return (bucket && Object.assign({}, bucket, {
+			amount: isPeriodic ? '$' + bucket.amount.toFixed(2) : '',
+			period: isPeriodic ? bucket.period + ' ' + bucket.periodUnit : '',
+			nextDate: isPeriodic ? bucket.nextDate.format('l') : '',
+			rate: isPeriodic ? '$' + (bucket.amount / days).toFixed(2) : '',
+			balance: '$' + bucket.balance.plus(isPeriodic ? bucket.amount.mul(age / days) : 0).toFixed(2)
+		})) || {};
+	};
+
+	renderAdd = options => {
+		const bucket = options.rowData;
+		return (<GlyphButton glyph="plus" onClick={this.handleAdd} />);
+	};
+
+	renderActions = options => {
+		const bucket = options.rowData;
 		return (<div>
-			{buckets}
-			<div className='container'>
-				<div style={{flex: 7}}></div>
-				<div><GlyphButton glyph="plus" onClick={this.props.onAdd} /></div>
-			</div>
+			<GlyphButton glyph="pencil-alt" onClick={this.handleEdit(bucket.id)} />
+			{'\u00A0'}
+			<GlyphButton glyph="trash" onClick={this.handleDelete(bucket.id)} />
 		</div>);
+	};
+
+	handleAdd = () => this.props.dispatch(editBucket(null));
+
+	handleEdit = id => {
+		return () => this.props.dispatch(editBucket(id));
+	};
+
+	handleDelete = id => {
+		return () => this.props.dispatch(deleteBucket(id));
+	};
+
+	render() {
+		return (<AutoSizer>
+			{({height, width}) => (
+				<Table
+						width={width}
+						height={height}
+						headerHeight={20}
+						rowHeight={30}
+						rowCount={(this.props.buckets || []).length}
+						rowGetter={this.rowGetter} >
+					<Column label='Name' dataKey='name' width={80} flexGrow={1} />
+					<Column label='Amount' dataKey='amount' width={80} flexGrow={1} />
+					<Column label='Period' dataKey='period' width={80} flexGrow={1} />
+					<Column label='Next Date' dataKey='nextDate' width={80} flexGrow={1} />
+					<Column label='Daily' dataKey='rate' width={80} flexGrow={1} />
+					<Column label='Balance' dataKey='balance' width={80} flexGrow={1} />
+					<Column
+						dataKey='id'
+						headerRenderer={this.renderAdd}
+						cellRenderer={this.renderActions}
+						width={20}
+						flexGrow={0.5} />
+				</Table>
+			)}
+		</AutoSizer>);
 	}
 }
 
@@ -24,8 +84,4 @@ const mapStateToProps = state => ({
 	buckets: getBuckets(state)
 });
 
-const mapDispatchToProps = dispatch => ({
-	onAdd: bucket => dispatch(editBucket(null))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(BucketList);
+export default connect(mapStateToProps)(BucketList);
